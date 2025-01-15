27e53c14142263fe29e274264b6abf69c3a9257e
Tomas Toledo <trtoledo1977@gmail.com>
2025-01-15 13:03:15 -0300

    async.js

---
+++ async.js
@@ -0,0 +1,24 @@
+
+
+function firstfn()  {
+    console.log("first fn start");
+    secondFn.log();
+    console.log("first fn finish")
+}
+
+function secondFn () {
+    console.log("SECOND");
+}
+
+function thirdFn () {
+    console.log("THIRD");
+}
+
+function fourthFn () {
+    console.log("FORTH");
+}
+
+function fifthFn () {
+    console.log("FIFTH");
+}
+
